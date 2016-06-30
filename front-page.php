a4f4f6c0703667414a051b56d0b12c41b5c6250c
Megan O'Neill <mo2638@columbia.edu>
2016-06-29 20:26:01 -0400

    adjust hire me section

--- front-page.php
+++ front-page.php
@@ -51,40 +51,14 @@ Template Name: Home
                 <div class="col-lg-8 col-lg-offset-2">
                     <!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19. -->
                     <!-- The form should work on most web servers, but if the form is not working you may need to configure your web server differently. -->
-                    <form name="sentMessage" id="contactForm" novalidate>
-                        <div class="row control-group">
-                            <div class="form-group col-xs-12 floating-label-form-group controls">
-                                <label>Name</label>
-                                <input type="text" class="form-control" placeholder="Name" id="name" required data-validation-required-message="Please enter your name.">
-                                <p class="help-block text-danger"></p>
-                            </div>
-                        </div>
-                        <div class="row control-group">
-                            <div class="form-group col-xs-12 floating-label-form-group controls">
-                                <label>Email Address</label>
-                                <input type="email" class="form-control" placeholder="Email Address" id="email" required data-validation-required-message="Please enter your email address.">
-                                <p class="help-block text-danger"></p>
-                            </div>
-                        </div>
-                        <div class="row control-group">
-                            <div class="form-group col-xs-12 floating-label-form-group controls">
-                                <label>Phone Number</label>
-                                <input type="tel" class="form-control" placeholder="Phone Number" id="phone" required data-validation-required-message="Please enter your phone number.">
-                                <p class="help-block text-danger"></p>
-                            </div>
-                        </div>
-                        <div class="row control-group">
-                            <div class="form-group col-xs-12 floating-label-form-group controls">
-                                <label>Message</label>
-                                <textarea rows="5" class="form-control" placeholder="Message" id="message" required data-validation-required-message="Please enter a message."></textarea>
-                                <p class="help-block text-danger"></p>
-                            </div>
-                        </div>
-                        <br>
-                        <div id="success"></div>
                         <div class="row">
+                            <div class="col-lg-12 ">
+                                <p>Do you need help setting up a website, building a website, or any other tech work? I'd love to work on your project! Shoot me an email with your project details and let's talk!</p>
+                            </div>
+                        </div>
+                        <div class="row email-row">
                             <div class="form-group col-xs-12">
-                                <button type="submit" class="btn btn-success btn-lg">Email Me</button>
+                                <a href="mailto:meganoneill38@gmail.com"><button type="submit" class="btn btn-success btn-lg" href="mailto:megan.oneill38@gmail.com">Email Me</button></a>
                             </div>
                         </div>
                     </form>
