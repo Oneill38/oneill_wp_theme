663e588a33651368db633663038da337fb8f2f45
Megan O'Neill <mo2638@columbia.edu>
2016-07-11 23:21:08 -0400

    fix tag display

---
+++ tag.php
@@ -0,0 +1,34 @@
+<?php get_header(); ?>
+
+<section>
+  <div class="container">
+    <div class="row">
+      <h3>Posts:</h3>
+      <div>
+       <?php $tagname = get_query_var('tag'); ?>
+       <?php $the_query = new WP_Query( "tag=$tagname&posts_per_page=-1" ); ?>
+       <?php if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+           <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+           <p><em><?php the_time('l, F jS, Y'); ?></em></p>
+             <div>
+               <?php the_excerpt(); ?>
+             </div>
+             <h4>Tags:</h4>
+             <ul class="page-list-tags">
+              <?php 
+                foreach(get_the_tags($post->ID) as $tag)
+                {
+                  echo '<li><a href="' . get_tag_link($tag->term_id) . '">' . $tag->name . '</a></li>';
+                }                
+              ?>
+             </ul>
+             <hr>
+      <?php endwhile; endif; ?>
+
+      </div>
+
+    </div>
+  </div>
+</section>
+
+<?php get_footer(); ?>
